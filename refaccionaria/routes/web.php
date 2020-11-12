7038109a117fdf4571155b0f183620f604ea0a5f
andreyhz <andrey_hz@outlook.com>
2020-11-11 21:29:45 -0600

    4. Definir las rutas del controlador

--- refaccionaria/routes/web.php
+++ refaccionaria/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para tabla Piezas
+Route::get('/piezas', 'App\Http\Controllers\PiezasController@index');
+Route::post('/store', 'App\Http\Controllers\PiezasController@store');
+Route::get('/destroy/{id}', 'App\Http\Controllers\PiezasController@destroy');
+Route::get('/edit/{id}', 'App\Http\Controllers\PiezasController@edit');
+Route::post('/update', 'App\Http\Controllers\PiezasController@update');
